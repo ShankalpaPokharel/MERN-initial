75caa002d7b202132eb966eb764bd0214af68a70
ShankalpaPokharel <pokharelshankalpa58@gmail.com>
2024-02-19 22:02:44 +0545

    nothing much

--- Basic understanding/Javascript/index.js
+++ Basic understanding/Javascript/index.js
@@ -30,12 +30,57 @@
 // console.log(`My name is ${name} ${lname}. I am ${age} years old.`)
 
 
-const list = ['ab','cd','ef','gh']
-console.log(list.length)
+// const list = ['ab','cd','ef','gh']
+// console.log(list.length)
 
-if(list.includes('ab')){
-    console.log("ab present")
+// if(list.includes('ab')){
+//     console.log("ab present")
+// }
+// else{
+//     console.log("Item doesn't exist in list")
+// }
+
+
+// Map 
+// const ages = [3,4,5,6,7]
+
+// const ag = ages.map(myfunction)
+// function myfunction(num){
+//     return num*10;
+// }
+// console.log(ag)
+
+// const persons = [
+//     {firstname : "Malcom", lastname: "Reynolds"},
+//     {firstname : "Kaylee", lastname: "Frye"},
+//     {firstname : "Jayne", lastname: "Cobb"}
+//   ];
+
+
+// const perso = persons.map(item=>{
+//     return item.firstname + item.lastname
+// })
+
+// console.log(perso)
+
+
+//map with id 
+// const fruits = ['apple','banana','mango','orange']
+
+// const items = fruits.map((item,index)=>{
+//     console.log(item,index)
+// })
+
+async function printing() {
+    for (let i =0;i<1000;i++){
+        console.log(i);
+    }
 }
-else{
-    console.log("Item doesn't exist in list")
-}
+function abc(){
+    for (let i = 97; i <= 122; i++) {
+        // Convert ASCII value to corresponding character and print
+        console.log(String.fromCharCode(i));
+    }}
+console.log("hi")
+printing()
+abc()
--- Mern_Mind/practice/express-practice-export-middleware/index.js
+++ Mern_Mind/practice/express-practice-export-middleware/index.js
@@ -18,33 +18,31 @@ app.get('/',(req,res)=>{
 })
 
 // get request
-app.get('/api/todos',(req,res)=>{
-    console.log("get api todos")
-    res.status(200).send(todo);
-})
+// app.get('/api/todos',(req,res)=>{
+//     console.log("get api todos")
+//     res.status(200).send(todo);
+// })
 
 // post request to send item
-app.post('/api/todos',checkAuthetication,checkValidRole,(req,res)=>{
-    console.log("post api todos")
-    const title = req.body.title
-    if (title === ""){
-        return res.status(400).send("Title field is require")
-    }
-    else if(todo.includes(title)){
-        return res.status(400).send("Title is already exist")
-    }
-    else{
-        todo.push(title);
-        return res.send("Update todos"+todo)
-    }
-})
+// app.post('/api/todos',checkAuthetication,checkValidRole,(req,res)=>{
+//     console.log("post api todos")
+//     const title = req.body.title
+//     if (title === ""){
+//         return res.status(400).send("Title field is require")
+//     }
+//     else if(todo.includes(title)){
+//         return res.status(400).send("Title is already exist")
+//     }
+//     else{
+//         todo.push(title);
+//         return res.send("Update todos"+todo)
+//     }
+// })
 
-app.delete('/api/todos/reset',checkAuthetication,checkValidRole,(req,res)=>{
-    todo=[]
-    return res.send(todo)
-})
-
-
+// app.delete('/api/todos/reset',checkAuthetication,checkValidRole,(req,res)=>{
+//     todo=[]
+//     return res.send(todo)
+// })
 
 
 // listening port
@@ -52,18 +50,86 @@ app.listen(8000,()=>{
     console.log("I am listening")
 })
 
-/*
-const arrays = ['a','b','c']
-// const item='b'
+let todos = [
+    // {
+    //     id:1,
+    //     title:"html",
+    //     statuss:false
+    // },
+    // {
+    //     id:2,
+    //     title:"css",
+    //     statuss:false
+    // }
+]
 
-if (arrays.includes('B')){
-    console.log("item in arrays")
-}
-else{
-    console.log("item doesn't exist in array")
-}
-*/
+app.get('/api/todos',(req,res)=>{
+    console.log("get api todos")
+    res.status(200).send(todos);
+})
 
+let id_no = 2
+app.post('/api/todos', (req,res)=>{
+    // removing the whiteshpace of input 
+    let new_title = req.body.title?.trim()
+    console.log("I got this",new_title)
+    if(!new_title){
+        return res.status(400).send("Empty field")
+    }
+
+    
+    // check input item has already present or not 
+    if(todos.some(item=>item.title == new_title)){
+      return res.status(400).send("Title already exist")  
+    }
+    // adding new item in list as a object form
+    else{
+        let newItem = {id:id_no+1,
+                title:req.body.title,
+                statuss: false}
+        todos.push(newItem)
+        id_no++;
+        res.status(200).send(todos)
+    }
+    
+})
+
+
+app.put('/api/todos/:id',(req,res)=>{
+
+    let title = req.body.title;
+    let statuss = req.body.statuss;
+    let input_id = req.params.id;
+    console.log(title,statuss, input_id)
+    for (let i=0;i<todos.length;i++){
+        // update base on id 
+        if(todos[i].id == input_id){
+            // todos[i].title=title;
+            todos[i].statuss=statuss;
+            return res.status(200).send(todos)
+        }  
+    }
+    return res.status(400).send("Bad request")
+    
+    
+})
+
+app.delete('/api/todos/:id',(req,res)=>{
+    console.log("id for delete",req.params.id)
+    let input_id = req.params.id;
+    if (todos.length==0){
+        return res.status(500).send("internal Server Error: Todo list is not found")
+    }
+    let todos_length = todos.length
+    // filter doesn't update so we assigen filter item in old filter variable 
+    todos = todos.filter(todo => todo.id != input_id)
+    if(todos_length != todos.length){
+    return res.status(200).send(todos)
+    }
+    else{
+        return res.status(400).send("Requested specific id is not found")
+    }
+})
 
 
 
