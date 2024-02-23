5b82a5dba18082f6b6a2096bddba22a06ebc0869
ShankalpaPokharel <pokharelshankalpa58@gmail.com>
2024-02-23 23:13:21 +0545

    express with db

--- Mern_Mind/Express/index.js
+++ Mern_Mind/Express/index.js
@@ -5,6 +5,8 @@ const app = express()
 // Using Node.js `require()`
 const mongoose = require('mongoose');
 
+app.use(express.json())
+
 mongoose.connect('mongodb://127.0.0.1:27017/bookStore')
   .then(() => console.log('Connected!'));
 
@@ -20,20 +22,36 @@ const BookSchema = new Schema({
 const Book = mongoose.model("Book",BookSchema) //write book in capital latter
 
 app.get('/api/books',(req,res)=>{
-    Book.find().then((data)=>{
+    Book.find({}).then((data)=>{
         console.log(data);
         res.send(data)
     })
-    res.send("list of todods")
+    // res.send("list of todods")
 })
 
-app.post("/api/books",(req,res)=>{
-    Book.create({title:"js",isbn:333}).then((book)={
-        res.send(book)
-    })
+app.post("/api/books",async(req,res)=>{
+    let {title,isbn}=req.body;
+    let book = await Book.create({title,isbn})
+    res.send(book)
 })
 
 app.listen(8000,()=>{
     console.log("server started")
 })
+
+
+app.put("/api/books/:_id",async(req,res)=>{
+    try{
+        let id = req.params._id
+        let book = await Book.findByIdAndUpdate(id,{title:"ruby & rails",isbn:111},{new:true})
+        res.send(book)
+    }
+    catch(err){
+        res.status(500).send({
+            msg:"Server error"
+        })
+    }
+})
+
+
 
